126a01d43b7cd22d3ad50f83f6dcde02b5770306
Matt Huntington <matt.huntington@gmail.com>
2020-09-23 10:51:58 -0400

    delete route

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootApplication
 @RestController
@@ -21,12 +24,18 @@ public class DemoApplication {
 
 	@GetMapping("/people")
 	public Iterable<Person> index() {
-	    return personRepository.findAll();
+		return personRepository.findAll();
 	}
 
 	@PostMapping(path="/people")
-	public Iterable<Person>  create (@RequestBody Person personData) {
+	public Iterable<Person> create (@RequestBody Person personData) {
 		personRepository.save(personData);
+		return personRepository.findAll();
+	}
+
+	@DeleteMapping(value = "/people/{id}")
+	public Iterable<Person> delete(@PathVariable int id) {
+		personRepository.deleteById(id);
 		return personRepository.findAll();
 	}
 }
