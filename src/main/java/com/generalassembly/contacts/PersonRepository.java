56b4f074c976e22bcb9b65b39dde96db22cd3116
Matt Huntington <matt.huntington@gmail.com>
2020-09-23 12:05:24 -0400

    cleanup

--- src/main/java/com/generalassembly/contacts/PersonRepository.java
+++ src/main/java/com/generalassembly/contacts/PersonRepository.java
@@ -4,9 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.generalassembly.contacts.Person;
 
-// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
-// CRUD refers Create, Read, Update, Delete
-
 public interface PersonRepository extends CrudRepository<Person, Integer> {
 
 }
