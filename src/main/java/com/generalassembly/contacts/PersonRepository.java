1b908b7c0d0ab9821fd2e810ef957cbf30c2b7db
Matt Huntington <matt.huntington@gmail.com>
2020-09-23 12:06:27 -0400

    cleanup

--- src/main/java/com/generalassembly/contacts/PersonRepository.java
+++ src/main/java/com/generalassembly/contacts/PersonRepository.java
@@ -2,8 +2,6 @@ package com.generalassembly.contacts;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.generalassembly.contacts.Person;
-
 public interface PersonRepository extends CrudRepository<Person, Integer> {
 
 }
