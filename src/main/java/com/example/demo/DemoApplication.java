3e7ab0b77244a73a95150db86f039927ec9da5cc
Matt Huntington <matt.huntington@gmail.com>
2020-09-23 10:42:29 -0400

    create route

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @SpringBootApplication
 @RestController
@@ -20,5 +22,11 @@ public class DemoApplication {
 	@GetMapping("/people")
 	public Iterable<Person> index() {
 	    return personRepository.findAll();
+	}
+
+	@PostMapping(path="/people")
+	public Iterable<Person>  create (@RequestBody Person personData) {
+		personRepository.save(personData);
+		return personRepository.findAll();
 	}
 }
